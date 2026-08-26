7200cbe1cf82f723c1acc70451ae2f60be8184b7
jay stanfield <jajen1099.js@gmail.com>
2026-08-26 06:31:01 -0400

    Convert weekend guide into Scene feature

--- site-plugins/chattanooga-music-scene-core/chattanooga-music-scene-core.php
+++ site-plugins/chattanooga-music-scene-core/chattanooga-music-scene-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Chattanooga Music Scene Weekend Feature
  * Description: Site-specific publishing tools for Chattanooga Music Scene.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Chattanooga Music Scene
  * Requires at least: 6.4
  * Requires PHP: 7.4
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CMS_CORE_VERSION', '0.1.0' );
+define( 'CMS_CORE_VERSION', '0.2.0' );
 define( 'CMS_CORE_FILE', __FILE__ );
 define( 'CMS_CORE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CMS_CORE_URL', plugin_dir_url( __FILE__ ) );
