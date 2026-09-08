36c2494b3e26ca1b36166c858504af4a64a040a7
jay stanfield <jajen1099.js@gmail.com>
2026-09-08 01:06:40 -0400

    Wire Events Manager deletion abilities

--- workbench/labs/chattanooga-cms-admin/candidate/includes/class-cmsa-plugin.php
+++ workbench/labs/chattanooga-cms-admin/candidate/includes/class-cmsa-plugin.php
@@ -17,6 +17,7 @@ final class CMSA_Plugin {
 	private $member_mutation_abilities;
 	private $events_manager_abilities;
 	private $events_manager_mutation_abilities;
+	private $events_manager_deletion_abilities;
 
 	public static function instance() {
 		if ( null === self::$instance ) {
@@ -66,10 +67,11 @@ final class CMSA_Plugin {
 			$this->member_abilities = new CMSA_Member_Abilities( $members );
 			$this->member_mutation_abilities = new CMSA_Member_Mutation_Abilities( new CMSA_Member_Mutations( $members ) );
 		}
-		if ( ! $this->events_manager_abilities || ! $this->events_manager_mutation_abilities ) {
+		if ( ! $this->events_manager_abilities || ! $this->events_manager_mutation_abilities || ! $this->events_manager_deletion_abilities ) {
 			$events = new CMSA_Events_Manager();
 			$this->events_manager_abilities = new CMSA_Events_Manager_Abilities( $events );
 			$this->events_manager_mutation_abilities = new CMSA_Events_Manager_Mutation_Abilities( new CMSA_Events_Manager_Mutations( $events ) );
+			$this->events_manager_deletion_abilities = new CMSA_Events_Manager_Deletion_Abilities( new CMSA_Events_Manager_Deletion( $events ) );
 		}
 
 		$this->abilities->register();
@@ -82,5 +84,6 @@ final class CMSA_Plugin {
 		$this->member_mutation_abilities->register();
 		$this->events_manager_abilities->register();
 		$this->events_manager_mutation_abilities->register();
+		$this->events_manager_deletion_abilities->register();
 	}
 }
