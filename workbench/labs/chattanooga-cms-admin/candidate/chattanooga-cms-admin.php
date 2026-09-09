a80d57ac9e56b5b0788a490d134f886e40ea2b69
jay stanfield <jajen1099.js@gmail.com>
2026-09-09 19:12:08 -0400

    Load universal standard resource reader

--- workbench/labs/chattanooga-cms-admin/candidate/chattanooga-cms-admin.php
+++ workbench/labs/chattanooga-cms-admin/candidate/chattanooga-cms-admin.php
@@ -40,6 +40,7 @@ require_once CMSA_DIR . 'includes/class-cmsa-events-manager-taxonomy.php';
 require_once CMSA_DIR . 'includes/class-cmsa-weekend-feature.php';
 require_once CMSA_DIR . 'includes/class-cmsa-woocommerce-products.php';
 require_once CMSA_DIR . 'includes/class-cmsa-marketplace-listings.php';
+require_once CMSA_DIR . 'includes/class-cmsa-universal-resource-reader.php';
 require_once CMSA_DIR . 'includes/class-cmsa-universal-control-plane.php';
 require_once CMSA_DIR . 'includes/class-cmsa-abilities.php';
 require_once CMSA_DIR . 'includes/class-cmsa-content-abilities.php';
