232c0198b259a12a064144d438f19a94036cbeb4
jay stanfield <jajen1099.js@gmail.com>
2026-09-09 01:47:39 -0400

    Restore Weekend Feature bootstrap before marketplace work

--- site-plugins/chattanooga-music-scene-core/chattanooga-music-scene-core.php
+++ site-plugins/chattanooga-music-scene-core/chattanooga-music-scene-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Chattanooga Music Scene Weekend Feature
  * Description: Site-specific publishing tools for Chattanooga Music Scene.
- * Version: 0.2.5
+ * Version: 0.2.1
  * Author: Chattanooga Music Scene
  * Requires at least: 6.4
  * Requires PHP: 7.4
@@ -13,16 +13,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CMS_CORE_VERSION', '0.2.5' );
+define( 'CMS_CORE_VERSION', '0.2.1' );
 define( 'CMS_CORE_FILE', __FILE__ );
 define( 'CMS_CORE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CMS_CORE_URL', plugin_dir_url( __FILE__ ) );
 
 require_once CMS_CORE_DIR . 'includes/class-cms-weekend-posts.php';
-require_once CMS_CORE_DIR . 'includes/class-cms-marketplace.php';
 
 CMS_Weekend_Posts::instance();
-CMS_Marketplace::instance();
 
 register_activation_hook( CMS_CORE_FILE, array( 'CMS_Weekend_Posts', 'activate' ) );
 register_deactivation_hook( CMS_CORE_FILE, array( 'CMS_Weekend_Posts', 'deactivate' ) );
