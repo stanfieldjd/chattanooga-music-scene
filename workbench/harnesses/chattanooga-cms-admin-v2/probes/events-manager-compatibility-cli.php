24afff641ea7138ea85b7bb73af791c8c232a7d3
jay stanfield <jajen1099.js@gmail.com>
2026-09-10 11:35:51 -0400

    Fix Events Manager compatibility probe syntax

--- workbench/harnesses/chattanooga-cms-admin-v2/probes/events-manager-compatibility-cli.php
+++ workbench/harnesses/chattanooga-cms-admin-v2/probes/events-manager-compatibility-cli.php
@@ -90,6 +90,7 @@ function cmsa_v2_em_extract_id( $value ) {
 			if ( isset( $value[ $key ] ) && is_numeric( $value[ $key ] ) && (int) $value[ $key ] > 0 ) {
 				return (int) $value[ $key ];
 			}
+		}
 		foreach ( $value as $child ) {
 			$id = cmsa_v2_em_extract_id( $child );
 			if ( $id > 0 ) {
