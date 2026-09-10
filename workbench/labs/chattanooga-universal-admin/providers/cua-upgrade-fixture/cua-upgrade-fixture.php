08c0fb097b8ac22f1ae5bbeec5e7a8b6bc699969
jay stanfield <jajen1099.js@gmail.com>
2026-09-09 22:07:45 -0400

    Add disposable upgrade fixture

---
+++ workbench/labs/chattanooga-universal-admin/providers/cua-upgrade-fixture/cua-upgrade-fixture.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: CUA Upgrade Fixture
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function cua_upgrade_fixture_version() {
+	return '1.0.0';
+}
