649a92afc803a61c17e58a318ddfc8e3b14bd793
jay stanfield <jajen1099.js@gmail.com>
2026-09-10 02:57:48 -0400

    Wire registered Settings API administration

--- workbench/labs/chattanooga-universal-admin/candidate/chattanooga-cms-admin.php
+++ workbench/labs/chattanooga-universal-admin/candidate/chattanooga-cms-admin.php
@@ -28,6 +28,7 @@ require_once CUA_DIR . 'includes/class-cua-platform-update-policy.php';
 require_once CUA_DIR . 'includes/class-cua-platform-package-lifecycle.php';
 require_once CUA_DIR . 'includes/class-cua-backups.php';
 require_once CUA_DIR . 'includes/class-cua-platform-core-maintenance.php';
+require_once CUA_DIR . 'includes/class-cua-platform-settings.php';
 require_once CUA_DIR . 'includes/class-cua-rest-bridge.php';
 require_once CUA_DIR . 'includes/class-cua-ability-bridge.php';
 
@@ -45,5 +46,6 @@ add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Package_Lifecycle', 'r
 add_action( 'wp_abilities_api_init', array( 'CUA_Audit', 'register_ability' ), 17 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Backups', 'register_abilities' ), 18 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Core_Maintenance', 'register_abilities' ), 19 );
+add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Settings', 'register_abilities' ), 20 );
 add_action( 'wp_abilities_api_init', array( 'CUA_REST_Bridge', 'register_external_bridges' ), 9998 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Ability_Bridge', 'register_external_bridges' ), 9999 );
