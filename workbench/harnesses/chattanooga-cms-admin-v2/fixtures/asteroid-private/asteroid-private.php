0ffe696234a6da365c4d3c3ffb03b472685ae25e
jay stanfield <jajen1099.js@gmail.com>
2026-09-09 23:34:36 -0400

    Add unsupported v2 fixture

---
+++ workbench/harnesses/chattanooga-cms-admin-v2/fixtures/asteroid-private/asteroid-private.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Asteroid Private Fixture
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function asteroid_private_fixture_value() {
+	return 'private-only';
+}
