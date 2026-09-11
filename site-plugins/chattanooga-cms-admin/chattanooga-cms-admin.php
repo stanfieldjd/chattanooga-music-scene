7a95b11e50304772c0563dafe65e9df76633f5b5
jay stanfield <jajen1099.js@gmail.com>
2026-09-11 11:12:11 -0400

    Bump repaired admin plugin to 1.0.2

--- site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
+++ site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Chattanooga CMS Admin
  * Description: Universal Chattanooga CMS Admin replacement using plugin-agnostic public WordPress contracts and verified intrinsic platform services.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Chattanooga Music Scene
  * Requires at least: 7.1
  * Requires PHP: 8.0
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'CUA_VERSION', '1.0.1' );
+define( 'CUA_VERSION', '1.0.2' );
 define( 'CUA_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once CUA_DIR . 'includes/class-cua-local-storage.php';
