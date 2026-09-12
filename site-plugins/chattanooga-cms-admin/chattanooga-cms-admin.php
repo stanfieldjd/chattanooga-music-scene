9771ba6496ea0401486a40b1cd9430ed2dd30e68
jay stanfield <jajen1099.js@gmail.com>
2026-09-12 07:13:37 -0400

    Register native media upload administration ability

--- site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
+++ site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
@@ -29,6 +29,7 @@ require_once CUA_DIR . 'includes/class-cua-platform-package-lifecycle.php';
 require_once CUA_DIR . 'includes/class-cua-backups.php';
 require_once CUA_DIR . 'includes/class-cua-platform-core-maintenance.php';
 require_once CUA_DIR . 'includes/class-cua-platform-settings.php';
+require_once CUA_DIR . 'includes/class-cua-platform-media.php';
 require_once CUA_DIR . 'includes/class-cua-rest-bridge.php';
 require_once CUA_DIR . 'includes/class-cua-ability-bridge.php';
 require_once CUA_DIR . 'includes/class-cua-bridge-gateway.php';
@@ -50,6 +51,7 @@ add_action( 'wp_abilities_api_init', array( 'CUA_Audit', 'register_ability' ), 1
 add_action( 'wp_abilities_api_init', array( 'CUA_Backups', 'register_abilities' ), 18 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Core_Maintenance', 'register_abilities' ), 19 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Settings', 'register_abilities' ), 20 );
+add_action( 'wp_abilities_api_init', array( 'CUA_Platform_Media', 'register_ability' ), 21 );
 add_action( 'wp_abilities_api_init', array( 'CUA_REST_Bridge', 'register_external_bridges' ), 9998 );
 add_action( 'wp_abilities_api_init', array( 'CUA_Ability_Bridge', 'register_external_bridges' ), 9999 );
 add_action( 'rest_api_init', array( 'CUA_MCP_Server', 'register_route' ) );
