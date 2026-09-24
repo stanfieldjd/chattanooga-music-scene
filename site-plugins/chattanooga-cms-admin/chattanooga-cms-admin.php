f5d3b1d758303f6cddd64e395737f5664ac8b6f1
jay stanfield <jajen1099.js@gmail.com>
2026-09-24 00:31:52 -0400

    Register MCP-specific REST CORS headers

--- site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
+++ site-plugins/chattanooga-cms-admin/chattanooga-cms-admin.php
@@ -63,6 +63,8 @@ add_action( 'wp_abilities_api_init', array( 'CUA_Ability_Bridge', 'register_exte
 add_action( 'wp_loaded', array( 'CUA_Ability_Bridge', 'prime_catalog_snapshot' ), 999999 );
 add_action( 'rest_api_init', array( 'CUA_MCP_Server', 'register_route' ) );
 add_action( 'rest_api_init', array( 'CUA_MCP_Diagnostics', 'register_routes' ), 20 );
+add_filter( 'rest_allowed_cors_headers', array( 'CUA_MCP_Server', 'allow_cors_request_headers' ), 10, 2 );
+add_filter( 'rest_exposed_cors_headers', array( 'CUA_MCP_Server', 'expose_cors_response_headers' ), 10, 2 );
 // Providers may register public abilities or REST routes after the initial
 // wp_abilities_api_init pass. Re-run the idempotent facade registration once
 // the REST registry is fully assembled, before MCP calls can arrive.
